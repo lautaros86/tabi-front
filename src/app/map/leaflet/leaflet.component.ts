d0ae8ab77b0601d678863882d816363afa7a9bb3
Lautaro Silva <lautaro.silva@despegar.com>
2022-12-18 02:04:10 -0300

    mapa lindo

--- src/app/map/leaflet/leaflet.component.ts
+++ src/app/map/leaflet/leaflet.component.ts
@@ -1,5 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {LatLng, latLng, marker, polygon, tileLayer} from "leaflet";
+import {LatLng, marker, polygon} from "leaflet";
+
+import * as L from 'leaflet';
+import 'leaflet.markercluster';
+import {Control} from 'leaflet';
+import LayersOptions = Control.LayersOptions;
+import {Barrio} from "../map.component";
 
 
 @Component({
@@ -11,27 +17,103 @@ export class LeafletComponent implements OnInit {
   @Input() center: number[] = [-34.92145, -57.95453];
   @Input() markers: number[][] = []
   @Input() polygons: number[][][][] = []
-  options = {}
-  layers: any  = []
-  puntos: any = []
-  ngOnInit(): void {
+  @Input() zonas: Barrio[] = []
+  areas: any = []
 
-    this.options = {
-      layers: [
-        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-      ],
-      zoom: 12,
-      center: latLng(-34.92145, -57.95453)
-    };
-
-    this.polygons.forEach( (part) => {
-          part.forEach( polygonData => {
-            var latlngs = polygonData.map( p => new LatLng(p[1], p[0]) )
-            this.layers.push(polygon(latlngs, {color: 'red'}))
-          })
+  // Open Street Map Definition
+  LAYER_OSM = {
+    id: 'openstreetmap',
+    name: 'Open Street Map',
+    enabled: false,
+    layer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: 'Open Street Map'
     })
+  };
 
-    this.markers.forEach((m) => this.layers.push(marker(new LatLng(m[0], m[1]))))
+  // Values to bind to Leaflet Directive
+  layersControlOptions: LayersOptions = {position: 'bottomright'};
+  baseLayers = {
+    'Open Street Map': this.LAYER_OSM.layer
+  };
+  options = {
+    zoom: 12,
+    center: L.latLng(-34.92145, -57.95453)
+  };
+
+  // Marker cluster stuff
+  markerClusterGroup: L.MarkerClusterGroup | undefined;
+  markerClusterData: L.Marker[] = [];
+  markerClusterOptions: L.MarkerClusterGroupOptions = {};
+
+  ngOnInit(): void {
+    this.refreshData();
+
+
+    const comparador: number[] = [0, 0, 0, 0, 0]
+
+    this.zonas.forEach((barrio) => {
+      var cantPersonas = barrio.personas_genero_fem + barrio.personas_genero_masc + barrio.personas_genero_otrx
+      if (barrio.id_renabap == 273 || barrio.nombre_barrio=='Santa Catalina') {
+        console.log("chequea")
+      }
+      var color = ''
+      switch (true) {
+        case (cantPersonas <= 3300):
+          comparador[0] = comparador[0] + 1
+          color = '#fb6a4a'
+          break;
+        case (3301 <= cantPersonas && cantPersonas <= 6600):
+          comparador[1] = comparador[1] + 1
+          color = '#ef3b2c'
+          break;
+        case (6601 <= cantPersonas && cantPersonas <= 9900):
+          comparador[2] = comparador[2] + 1
+          color = '#cb181d'
+          break;
+        case (9901 <= cantPersonas && cantPersonas <= 13200):
+          comparador[3] = comparador[3] + 1
+          color = '#a50f15'
+          break;
+        default:
+          comparador[4] = comparador[4] + 1
+          color = '#67000d'
+          break;
+      }
+      var popup = L.popup()
+        .setContent(`
+        <p><b><u>${barrio.nombre_barrio}</u></b></p>
+        <p><b>Población: </b>${cantPersonas}</p>
+        <p><b>Viviendas: </b>${barrio.cantidad_viviendas_aproximadas}</p>
+        <p><b>Familias: </b>${barrio.cantidad_familias_aproximada}</p>
+        `)
+      barrio.poligono.forEach(polygonData => {
+        var latlngs = polygonData.map(p => new LatLng(p[1], p[0]))
+        this.areas.push(polygon(latlngs, {color: color, fillOpacity: 0.6}).bindPopup(popup))
+      })
+      console.log(comparador)
+    })
+  }
+
+  markerClusterReady(group: L.MarkerClusterGroup) {
+
+    this.markerClusterGroup = group;
+
+  }
+
+  refreshData(): void {
+    this.markerClusterData = this.generateData(1000);
+  }
+
+  generateData(count: number): L.Marker[] {
+
+    const data: L.Marker[] = []
+    this.markers.forEach((m) => {
+      var esta = data.find(e => e.getLatLng().lat == m[0] && e.getLatLng().lng == m[1])
+      if (!esta)
+        data.push(marker(new LatLng(m[0], m[1])))
+    });
+    return data;
 
   }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import { BARRIOS } from './BarriosPuntos'
-import { PUNTOS } from './Puntos'
+import {BARRIOS_PUNTOS} from './BarriosPuntos'
+import {PUNTOS} from './Puntos'
 import {latLng, tileLayer} from "leaflet";
+
+export interface Barrio {
+  personas_genero_fem: number
+  id_renabap: number
+  cantidad_familias_aproximada: number
+  nombre_barrio: string
+  cantidad_viviendas_aproximadas: number
+  personas_genero_masc: number
+  geometry: string
+  personas_genero_otrx: number
+  poligono: number[][][]
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -11,31 +24,39 @@ export class MapComponent implements OnInit {
   center: number[] = [];
   markers: number[][] = [];
   polygons: number[][][][] = [];
-  barrios = BARRIOS
+  zonas: Barrio[] = []
+  barrios = BARRIOS_PUNTOS
   puntos = PUNTOS
+
   ngOnInit() {
     this.center = [-34.92145, -57.95453]
     this.cargarPuntos()
     this.cargarBarrios()
   }
+
   cargarPuntos() {
-    this.markers = this.puntos.puntos.map( p => [p.latitud, p.longitud])
+    this.markers = this.puntos.puntos.map(p => [p.latitud, p.longitud])
   }
 
   cargarBarrios() {
-    this.polygons = this.barrios.data.map( barrio => {
-       try {
-         return JSON.parse(barrio.geometry
-           .replace('MULTIPOLYGON ', '')
-           .replaceAll('(', '[')
-           .replaceAll(')', ']')
-           .replaceAll(', ', '],[')
-           .replaceAll(' ', ', ')
-           .replaceAll(']]],[[[', ']],[[')
-         )
-       } catch (e ) {
-         console.log("fallo el mapeo de " + barrio)
-       }
+    this.zonas = this.barrios.data.map(barrio => {
+        var poligono = []
+      if(barrio.id_renabap == 273) {
+        console.log("frena")
+      }
+        try {
+          poligono = JSON.parse(barrio.geometry
+            .replace('MULTIPOLYGON ', '')
+            .replaceAll('(', '[')
+            .replaceAll(')', ']')
+            .replaceAll(', ', '],[')
+            .replaceAll(' ', ', ')
+            .replaceAll(']]],[[[', ']],[[')
+          )
+        } catch (e) {
+          console.log("fallo el mapeo de " + barrio)
+        }
+        return {...barrio, poligono}
       }
     )
 
